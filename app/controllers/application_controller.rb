3c5b04b6c6cf7cb5c73fdef46a305bea47eaae4b
Charles Swaleh <charlswall@live.com>
2023-04-03 14:17:45 +0300

    Created the /bakeries route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, "application/json"
 
   # add routes
-
+  get "/bakeries" do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 end
