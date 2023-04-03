8c3ef74122cbc71d20e0447ae8291d68873e4dfa
Charles Swaleh <charlswall@live.com>
2023-04-03 14:22:01 +0300

    Created the /baked_goods/by_price route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,9 @@ class ApplicationController < Sinatra::Base
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
   end
+
+  get "/baked_goods/by_price" do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
 end
